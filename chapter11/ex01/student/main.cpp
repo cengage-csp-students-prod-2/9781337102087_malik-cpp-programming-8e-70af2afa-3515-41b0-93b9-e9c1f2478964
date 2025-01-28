ae3f485d62a79514492c0fc707aa47dfb1c256e6
Teurys0427 <tperaltapolanco@mymail.lccc.edu>
2025-01-28 13:28:23 +0000

    Testing Tue Jan 28 2025 13:28:23 GMT+0000 (Coordinated Universal Time)

--- chapter11/ex01/student/main.cpp
+++ chapter11/ex01/student/main.cpp
@@ -1,8 +1,96 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
+class clockType {
+protected:
+    int hours;
+    int minutes;
+    int seconds;
+
+public:
+    clockType(int h = 0, int m = 0, int s = 0) {
+        setTime(h, m, s);
+    }
+
+    void setTime(int h, int m, int s) {
+        if (0 <= h && h < 24)
+            hours = h;
+        else
+            hours = 0;
+
+        if (0 <= m && m < 60)
+            minutes = m;
+        else
+            minutes = 0;
+
+        if (0 <= s && s < 60)
+            seconds = s;
+        else
+            seconds = 0;
+    }
+
+    void getTime(int &h, int &m, int &s) const {
+        h = hours;
+        m = minutes;
+        s = seconds;
+    }
+
+    void printTime() const {
+        if (hours < 10)
+            cout << "0";
+        cout << hours << ":";
+        if (minutes < 10)
+            cout << "0";
+        cout << minutes << ":";
+        if (seconds < 10)
+            cout << "0";
+        cout << seconds;
+    }
+};
+
+class extClockType : public clockType {
+private:
+    string timeZone;
+
+public:
+    extClockType(int h = 0, int m = 0, int s = 0, const string &tz = "UTC") : clockType(h, m, s) {
+        timeZone = tz;
+    }
+
+    void setTimeZone(const string &tz) {
+        timeZone = tz;
+    }
+
+    string getTimeZone() const {
+        return timeZone;
+    }
+
+    void printTime() const {
+        clockType::printTime();
+        cout << " " << timeZone;
+    }
+};
+
 int main() {
-    // Write your main here
+    // Create an object of extClockType with default time and time zone
+    extClockType clock1;
+    cout << "Default clock: ";
+    clock1.printTime();
+    cout << endl;
+
+    // Create an object of extClockType with custom time and time zone
+    extClockType clock2(12, 30, 45, "EST");
+    cout << "Custom clock: ";
+    clock2.printTime();
+    cout << endl;
+
+    // Modify the time and time zone of clock1
+    clock1.setTime(9, 15, 0);
+    clock1.setTimeZone("PST");
+    cout << "Modified clock: ";
+    clock1.printTime();
+    cout << endl;
+
     return 0;
 }
