118bd10495fc738fd236be43ee356e87b303f7a9
Teurys0427 <tperaltapolanco@mymail.lccc.edu>
2025-02-01 13:38:39 +0000

    Testing Sat Feb 01 2025 13:38:39 GMT+0000 (Coordinated Universal Time)

---
+++ chapter11/ex01/student/extClockTypeImp.cpp
@@ -0,0 +1,20 @@
+#include "extClockType.h"
+#include <iostream>
+using namespace std;
+
+extClockType::extClockType(int h, int m, int s, const string &tz) : clockType(h, m, s) {
+    timeZone = tz;
+}
+
+void extClockType::setTimeZone(const string &tz) {
+    timeZone = tz;
+}
+
+string extClockType::getTimeZone() const {
+    return timeZone;
+}
+
+void extClockType::printTime() const {
+    clockType::printTime();
+    cout << " " << timeZone;
+}
--- chapter11/ex01/student/main.cpp
+++ chapter11/ex01/student/main.cpp
@@ -1,8 +1,26 @@
+#include "extClockType.h"
 #include <iostream>
-
 using namespace std;
 
 int main() {
-    // Write your main here
+    // Create an object of extClockType with default time and time zone
+    extClockType clock1;
+    cout << "Default clock: ";
+    clock1.printTime();
+    cout << endl;
+
+    // Create an object of extClockType with custom time and time zone
+    extClockType clock2(12, 30, 45, "EST");
+    cout << "Custom clock: ";
+    clock2.printTime();
+    cout << endl;
+
+    // Modify the time and time zone of clock1
+    clock1.setTime(9, 15, 0);
+    clock1.setTimeZone("PST");
+    cout << "Modified clock: ";
+    clock1.printTime();
+    cout << endl;
+
     return 0;
 }
