ce7757a91edc6a5bad5760daa00e92868e43a00e
Teurys0427 <tperaltapolanco@mymail.lccc.edu>
2025-02-01 01:57:12 +0000

    Testing Sat Feb 01 2025 01:57:12 GMT+0000 (Coordinated Universal Time)

--- chapter11/ex01/student/main.cpp
+++ chapter11/ex01/student/main.cpp
@@ -3,24 +3,8 @@
 using namespace std;
 
 int main() {
-    // Test default constructor
-    extClockType clock1;
-    cout << "Default clock: ";
-    clock1.printTime();
+    extClockType clock(12, 30, 45, "EST");
+    clock.printTime();
     cout << endl;
-
-    
-    extClockType clock2(12, 30, 45, "EST");
-    cout << "Custom clock: ";
-    clock2.printTime();
-    cout << endl;
-
-    
-    clock1.setTime(9, 15, 0);
-    clock1.setTimeZone("PST");
-    cout << "Modified clock: ";
-    clock1.printTime();
-    cout << endl;
-
     return 0;
 }
