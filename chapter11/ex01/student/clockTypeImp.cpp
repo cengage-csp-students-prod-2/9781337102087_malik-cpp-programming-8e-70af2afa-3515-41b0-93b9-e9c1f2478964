99864f7992c59d349e98d419b0eeac0dc2cefc08
Teurys0427 <tperaltapolanco@mymail.lccc.edu>
2025-02-01 13:29:11 +0000

    Reset Exercise Sat Feb 01 2025 13:29:11 GMT+0000 (Coordinated Universal Time)

--- chapter11/ex01/student/clockTypeImp.cpp
+++ chapter11/ex01/student/clockTypeImp.cpp
@@ -1,40 +1,108 @@
+
+//Implementation File for the class clockType
+ 
+#include <iostream>
 #include "clockType.h"
+  
+using namespace std;
+ 
+void clockType::setTime(int hours, int minutes, int seconds)
+{
+    if (0 <= hours && hours < 24)
+        hr = hours;
+    else 
+        hr = 0; 
 
-clockType::clockType(int h, int m, int s) {
-    setTime(h, m, s);
+    if (0 <= minutes && minutes < 60)
+        min = minutes;
+    else 
+        min = 0;
+
+    if (0 <= seconds && seconds < 60)
+        sec = seconds;
+    else 
+        sec = 0;
 }
 
-void clockType::setTime(int h, int m, int s) {
-    if (0 <= h && h < 24)
-        hours = h;
-    else
-        hours = 0;
-
-    if (0 <= m && m < 60)
-        minutes = m;
-    else
-        minutes = 0;
-
-    if (0 <= s && s < 60)
-        seconds = s;
-    else
-        seconds = 0;
+void clockType::getTime(int& hours, int& minutes, 
+                        int& seconds) const
+{
+    hours = hr;
+    minutes = min;
+    seconds = sec;
 }
 
-void clockType::getTime(int &h, int &m, int &s) const {
-    h = hours;
-    m = minutes;
-    s = seconds;
+void clockType::incrementHours()
+{
+    hr++;
+    if (hr > 23)
+        hr = 0;
 }
 
-void clockType::printTime() const {
-    if (hours < 10)
+void clockType::incrementMinutes()
+{
+    min++;
+    if (min > 59)
+    {
+        min = 0;
+        incrementHours();
+    }
+}
+
+void clockType::incrementSeconds()
+{
+    sec++;
+
+    if (sec > 59)
+    {
+        sec = 0;
+        incrementMinutes();
+    }
+}
+
+void clockType::printTime() const
+{
+    if (hr < 10)
         cout << "0";
-    cout << hours << ":";
-    if (minutes < 10)
+    cout << hr << ":";
+
+    if (min < 10)
         cout << "0";
-    cout << minutes << ":";
-    if (seconds < 10)
+    cout << min << ":";
+
+    if (sec < 10)
         cout << "0";
-    cout << seconds;
+    cout << sec;
+}
+
+bool clockType::equalTime(const clockType& otherClock) const
+{
+    return (hr == otherClock.hr 
+            && min == otherClock.min 
+            && sec == otherClock.sec);
+}
+
+clockType::clockType(int hours, int minutes, int seconds)
+{
+    if (0 <= hours && hours < 24)
+        hr = hours;
+    else 
+        hr = 0;
+
+    if (0 <= minutes && minutes < 60)
+        min = minutes;
+    else 
+        min = 0;
+
+    if (0 <= seconds && seconds < 60)
+        sec = seconds;
+    else 
+    sec = 0;
+}
+
+clockType::clockType()  //default constructor
+{
+    hr = 0;
+    min = 0;
+    sec = 0;
 }
