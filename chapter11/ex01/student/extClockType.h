f135546dd483564a0457df7e324c9c46e67e1558
Teurys0427 <tperaltapolanco@mymail.lccc.edu>
2025-02-02 14:14:14 +0000

    Testing Sun Feb 02 2025 14:14:14 GMT+0000 (Coordinated Universal Time)

---
+++ chapter11/ex01/student/extClockType.h
@@ -0,0 +1,19 @@
+#ifndef EXTCLOCKTYPE_H
+#define EXTCLOCKTYPE_H
+
+#include "clockType.h"
+#include <string>
+using namespace std;
+
+class extClockType : public clockType {
+private:
+    string timeZone;
+
+public:
+    extClockType(int h = 0, int m = 0, int s = 0, const string &tz = "UTC");
+    void setTimeZone(const string &tz);
+    string getTimeZone() const;
+    void printTime() const override; // Ensure it properly overrides printTime
+};
+
+#endif
--- chapter11/ex01/student/main.cpp
+++ chapter11/ex01/student/main.cpp
@@ -1,8 +1,26 @@
+#include "extClockType.h"
 #include <iostream>
-
 using namespace std;
 
 int main() {
-    // Write your main here
+    // Test default constructor
+    extClockType clock1;
+    cout << "Default clock: ";
+    clock1.printTime();
+    cout << endl;
+
+    // Test custom initialization
+    extClockType clock2(12, 30, 45, "EST");
+    cout << "Custom clock: ";
+    clock2.printTime();
+    cout << endl;
+
+    // Modify clock1's time and time zone
+    clock1.setTime(9, 15, 0);
+    clock1.setTimeZone("PST");
+    cout << "Modified clock: ";
+    clock1.printTime();
+    cout << endl;
+
     return 0;
 }
