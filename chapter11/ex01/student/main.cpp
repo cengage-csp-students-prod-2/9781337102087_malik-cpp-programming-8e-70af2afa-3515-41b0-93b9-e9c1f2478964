dd117549dd05b54d29b0edc9304c720739408a4c
Teurys0427 <tperaltapolanco@mymail.lccc.edu>
2025-02-04 00:53:01 +0000

    Reset Exercise Tue Feb 04 2025 00:53:01 GMT+0000 (Coordinated Universal Time)

--- chapter11/ex01/student/main.cpp
+++ chapter11/ex01/student/main.cpp
@@ -1,26 +1,8 @@
-#include "extClockType.h"
 #include <iostream>
+
 using namespace std;
 
 int main() {
-    // Test default constructor
-    extClockType clock1;
-    cout << "Default clock: ";
-    clock1.printTime();
-    cout << endl;
-
-    
-    extClockType clock2(12, 30, 45, "EST");
-    cout << "Custom clock: ";
-    clock2.printTime();
-    cout << endl;
-
-    
-    clock1.setTime(9, 15, 0);
-    clock1.setTimeZone("PST");
-    cout << "Modified clock: ";
-    clock1.printTime();
-    cout << endl;
-
+    // Write your main here
     return 0;
 }
