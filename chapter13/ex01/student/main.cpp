e6e7cd0dc5c3f608201c77fd44eb0da0c3427d2d
Teurys0427 <tperaltapolanco@mymail.lccc.edu>
2025-02-14 01:59:12 +0000

    Reset Exercise Fri Feb 14 2025 01:59:12 GMT+0000 (Coordinated Universal Time)

--- chapter13/ex01/student/main.cpp
+++ chapter13/ex01/student/main.cpp
@@ -1,35 +1,8 @@
 #include <iostream>
-#include "rectangleType.h"
 
 using namespace std;
 
 int main() {
-    rectangleType rect1(5, 10);
-    rectangleType rect2(3, 6);
-    rectangleType rect3(5, 10);
-
-    cout << "Initial Rectangles:" << endl;
-    cout << rect1 << endl;
-    cout << rect2 << endl;
-
-    cout << "\nIncrement rect1: " << ++rect1 << endl;
-    cout << "Post-increment rect2: " << rect2++ << endl;
-    cout << "After post-increment: " << rect2 << endl;
-
-    cout << "\nDecrement rect1: " << --rect1 << endl;
-    cout << "Post-decrement rect2: " << rect2-- << endl;
-    cout << "After post-decrement: " << rect2 << endl;
-
-    rectangleType rect4 = rect1 - rect2;
-    cout << "\nAfter subtracting rect2 from rect1: " << rect4 << endl;
-
-    cout << "\nComparing Rectangles:" << endl;
-    cout << "Is rect1 == rect3? " << (rect1 == rect3 ? "Yes" : "No") << endl;
-    
-    rectangleType rect5;
-    cout << "\nEnter dimensions for a new rectangle:" << endl;
-    cin >> rect5;
-    cout << "You entered: " << rect5 << endl;
-
+    // Write your main here
     return 0;
 }
